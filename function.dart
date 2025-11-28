79f835e807b3d734ae1986e08f077845ae438ae0
Rakibul Hasan Sajib <118044314+rakibsojib1@users.noreply.github.com>
2025-11-28 20:14:58 +0600

    function added

---
+++ function.dart
@@ -0,0 +1,37 @@
+main() {
+  greetings();
+  print(calculate(5, 6, "+"));
+  print(calculate(5, 6, "*"));
+  print(known(name: 'Rakib'));
+  print(known(name: 'Rakib', age: 8));
+}
+
+void greetings() {
+  print("Hello World!");
+}
+
+int calculate(int a, int b, String operation) {
+  if (operation == '+') {
+    return a + b;
+  } else if (operation == '-') {
+    return a - b;
+  } else if (operation == '*') {
+    return a * b;
+  } else if (operation == '/') {
+    return a ~/ b; // Integer division
+  } else {
+    throw ArgumentError('Invalid operation: $operation');
+  }
+}
+
+String known({String? name, int? age}) {
+  if (age == 8) {
+    return "Are you ";
+  }
+  switch (name) {
+    case "Rakib" || "Hasan":
+      return "Known?";
+    default:
+      return "Unknown?";
+  }
+}
